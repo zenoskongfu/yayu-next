800789b00e3e24fab58cedf808e4ddd91850607b
zenos <zenos.chen@foxmail.com>
2025-02-22 20:46:58 +0800

    add pages

---
+++ 手写ssr/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export async function getServerSideProps() {
+	const res = await fetch("https://api.thecatapi.com/v1/images/search");
+	const cat = await res.json();
+	return { props: { cat } };
+}
+
+export default function Page({ cat }) {
+	console.log("cat: ", cat);
+	return <img src={cat[0].url} width='200'></img>;
+}
